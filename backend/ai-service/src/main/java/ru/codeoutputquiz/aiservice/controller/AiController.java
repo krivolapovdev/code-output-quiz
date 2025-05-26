c9cde2419210550166ab10925637c75e7fa2c99e
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-26 23:05:04 +0600

    Create api-gateway

--- backend/ai-service/src/main/java/ru/codeoutputquiz/aiservice/controller/AiController.java
+++ backend/ai-service/src/main/java/ru/codeoutputquiz/aiservice/controller/AiController.java
@@ -1,12 +1,24 @@
 package ru.codeoutputquiz.aiservice.controller;
 
+import java.time.Duration;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/api/v1/ai")
 public class AiController {
   @GetMapping
-  public String hello() {
-    return "Hello World";
+  public Mono<String> hello() {
+    return Mono.just("Hello World");
+  }
+
+  @GetMapping("/test")
+  public Flux<String> test() {
+    return Flux.fromIterable(List.of("Product A", "Product B", "Product C"))
+        .delayElements(Duration.ofSeconds(1));
   }
 }

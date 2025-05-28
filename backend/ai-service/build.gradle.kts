dcea96e0cc049c8b3458ac383203902fabcf8b84
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-28 19:04:45 +0600

    Create ai-service.yml for config-server

--- backend/ai-service/build.gradle.kts
+++ backend/ai-service/build.gradle.kts
@@ -23,6 +23,7 @@ extra["springCloudVersion"] = "2025.0.0-RC1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")

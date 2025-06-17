e45f9d7356debc8833bbe5306849a1eccc40f532
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-06-17 18:34:39 +0500

    Update CustomMenu to include USER_KEY in defaultOpenKeys

--- frontend/src/widgets/CustomMenu/ui/index.tsx
+++ frontend/src/widgets/CustomMenu/ui/index.tsx
@@ -1,14 +1,14 @@
 import { Menu } from 'antd';
 
 import { menuItems } from '../config';
-import { DIFFICULTY_KEY, JAVA_KEY, LANGUAGE_KEY, PROGRAMMING_KEY } from '../consts';
+import { DIFFICULTY_KEY, JAVA_KEY, LANGUAGE_KEY, PROGRAMMING_KEY, USER_KEY } from '../consts';
 
 export const CustomMenu = () => {
   return (
     <Menu
       style={{ width: 256 }}
       defaultSelectedKeys={JAVA_KEY}
-      defaultOpenKeys={[PROGRAMMING_KEY, LANGUAGE_KEY, DIFFICULTY_KEY]}
+      defaultOpenKeys={[PROGRAMMING_KEY, LANGUAGE_KEY, DIFFICULTY_KEY, USER_KEY]}
       mode='inline'
       items={menuItems}
       className='min-h-[100vh] overflow-y-auto'

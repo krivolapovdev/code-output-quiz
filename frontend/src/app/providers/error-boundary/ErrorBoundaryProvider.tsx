bdfd2f85f95014485e5e15880ddb44854b223365
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-06-17 19:43:40 +0500

    Add `ErrorBoundaryProvider.tsx` component and update exports

--- frontend/src/app/providers/error-boundary/ErrorBoundaryProvider.tsx
+++ frontend/src/app/providers/error-boundary/ErrorBoundaryProvider.tsx
@@ -1,9 +1,11 @@
 import { ErrorBoundary } from 'react-error-boundary';
 
+import { ErrorFallback } from '@/shared/ui/error-fallback';
+
 type ErrorBoundaryProviderProps = {
   children: React.ReactNode;
 };
 
 export const ErrorBoundaryProvider = ({ children }: ErrorBoundaryProviderProps) => {
-  return <ErrorBoundary fallback={<div>Something went wrong...</div>}>{children}</ErrorBoundary>;
+  return <ErrorBoundary fallbackRender={ErrorFallback}>{children}</ErrorBoundary>;
 };

19f4990e78e9bb612c1183cf1d4b0bd7ce69f270
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-06-19 18:05:26 +0500

    Update import path for App and adjust Lefthook configuration

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,12 @@
+import "@/app/styles/index.css";
+
+import {ErrorBoundaryProvider} from "@/app/providers/error-boundary/ErrorBoundaryProvider";
+import {AppRouter} from "@/app/routes/app-router/AppRouter";
+
+export const App = () => {
+  return (
+    <ErrorBoundaryProvider>
+      <AppRouter/>
+    </ErrorBoundaryProvider>
+  );
+};

9aa59ff43c5db6df5dd092a29d61f5e4bebe35db
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-06-26 19:15:05 +0500

    Remove singleton instance from QuizMapper

--- backend/quiz-service/src/main/java/io/github/krivolapovdev/codeoutputquiz/quizservice/mapper/QuizMapper.java
+++ backend/quiz-service/src/main/java/io/github/krivolapovdev/codeoutputquiz/quizservice/mapper/QuizMapper.java
@@ -3,11 +3,8 @@ package io.github.krivolapovdev.codeoutputquiz.quizservice.mapper;
 import io.github.krivolapovdev.codeoutputquiz.quizservice.entity.Quiz;
 import io.github.krivolapovdev.codeoutputquiz.quizservice.response.QuizResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface QuizMapper {
-  QuizMapper INSTANCE = Mappers.getMapper(QuizMapper.class);
-
   QuizResponse toResponse(Quiz quiz);
 }

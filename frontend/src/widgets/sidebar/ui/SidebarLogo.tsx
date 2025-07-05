99f5ad7fc526d806f604e6458d2a2cc9b9053d89
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-07-05 14:49:09 +0500

    Replace anchor tag with button in SidebarLogo for improved navigation handling

--- frontend/src/widgets/sidebar/ui/SidebarLogo.tsx
+++ frontend/src/widgets/sidebar/ui/SidebarLogo.tsx
@@ -1,17 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import LogoIcon from "../assets/icons/logo.svg";
 
 export const SidebarLogo = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="inline-flex size-16 items-center justify-center">
-      <a
-        href="/"
-        className="grid size-10 place-content-center rounded-lg bg-gray-100 text-xs text-gray-600"
+      <button
+        type="button"
+        onClick={() => navigate("/")}
+        className="grid size-10 place-content-center rounded-lg bg-gray-100 text-xs text-gray-600 cursor-pointer"
       >
         <img
           src={LogoIcon}
           alt="Code Output Quiz"
         />
-      </a>
+      </button>
     </div>
   );
 };

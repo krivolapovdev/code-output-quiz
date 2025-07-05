8032009337f8042b141c3c02304e855ad3e716b8
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-07-05 10:08:03 +0500

    Delegate timestamp handling to PostgreSQL

--- backend/auth-service/src/main/java/io/github/krivolapovdev/codeoutputquiz/authservice/entity/User.java
+++ backend/auth-service/src/main/java/io/github/krivolapovdev/codeoutputquiz/authservice/entity/User.java
@@ -4,9 +4,7 @@ import io.github.krivolapovdev.codeoutputquiz.authservice.enums.UserRoles;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -24,11 +22,9 @@ public class User {
   @Column("role")
   private UserRoles role;
 
-  @CreatedDate
   @Column("created_at")
   private OffsetDateTime createdAt;
 
-  @LastModifiedDate
   @Column("updated_at")
   private OffsetDateTime updatedAt;
 

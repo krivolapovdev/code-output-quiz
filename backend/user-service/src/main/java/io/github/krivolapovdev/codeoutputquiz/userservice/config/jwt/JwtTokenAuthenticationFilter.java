b284e422fb19b68bf13b3404353e9ed200dce5ef
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-07-05 16:51:00 +0500

    Refactor JWT token extraction methods and add error handling for invalid tokens

--- backend/user-service/src/main/java/io/github/krivolapovdev/codeoutputquiz/userservice/config/jwt/JwtTokenAuthenticationFilter.java
+++ backend/user-service/src/main/java/io/github/krivolapovdev/codeoutputquiz/userservice/config/jwt/JwtTokenAuthenticationFilter.java
@@ -2,10 +2,13 @@ package io.github.krivolapovdev.codeoutputquiz.userservice.config.jwt;
 
 import io.github.krivolapovdev.codeoutputquiz.userservice.security.jwt.JwtTokenProvider;
 import io.github.krivolapovdev.codeoutputquiz.userservice.util.JwtUtils;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -18,14 +21,19 @@ public class JwtTokenAuthenticationFilter implements WebFilter {
   @Override
   public @NonNull Mono<Void> filter(
       @NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
-    return JwtUtils.extractToken(
+    return JwtUtils.extractAccessToken(
             exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
-        .map(tokenProvider::getAuthentication)
         .map(
-            authentication ->
-                chain
+            token -> {
+              try {
+                var auth = tokenProvider.getAuthentication(token);
+                return chain
                     .filter(exchange)
-                    .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication)))
+                    .contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth));
+              } catch (JwtException ex) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid JWT", ex);
+              }
+            })
         .orElseGet(() -> chain.filter(exchange));
   }
 }

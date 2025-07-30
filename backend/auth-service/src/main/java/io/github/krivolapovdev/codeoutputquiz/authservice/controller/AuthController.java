5d87b3874893623ae3cd404ee8e19780c5109c81
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-07-30 22:40:46 +0500

    Remove unused secured endpoint from `AuthController`

--- backend/auth-service/src/main/java/io/github/krivolapovdev/codeoutputquiz/authservice/controller/AuthController.java
+++ backend/auth-service/src/main/java/io/github/krivolapovdev/codeoutputquiz/authservice/controller/AuthController.java
@@ -6,9 +6,7 @@ import io.github.krivolapovdev.codeoutputquiz.authservice.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +18,6 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class AuthController {
   private final AuthService authService;
-
-  @GetMapping("/secure")
-  @PreAuthorize("hasRole('USER')")
-  public Mono<String> securedEndpoint() {
-    return Mono.just("You are authorized!");
-  }
 
   @PostMapping("/register")
   public Mono<ResponseEntity<AuthResponse>> register(@Valid @RequestBody AuthRequest authRequest) {

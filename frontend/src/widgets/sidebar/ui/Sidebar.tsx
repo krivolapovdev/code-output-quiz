2abdd4c5d323f8c583c13fb5b3a2c19815438a84
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-08-09 21:15:17 +0500

    Remove unnecessary height restriction from Sidebar component

--- frontend/src/widgets/sidebar/ui/Sidebar.tsx
+++ frontend/src/widgets/sidebar/ui/Sidebar.tsx
@@ -4,7 +4,7 @@ import { SidebarNavSection } from "./SidebarNavSection";
 
 export const Sidebar = () => {
   return (
-    <div className="flex h-screen w-16 flex-col justify-between border-e border-gray-100 bg-white z-10">
+    <div className="flex w-16 flex-col justify-between border-e border-gray-100 bg-white z-10">
       <div>
         <SidebarLogo />
         <SidebarNavSection />
